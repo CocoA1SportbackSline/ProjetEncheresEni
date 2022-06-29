0a5fb2d814b86f42173f825cd75f6f5827e68a15
asuel2020 <asuel2020@12SE14-56LRZS2.ad.campus-eni.fr>
2022-06-29 11:28:33 +0200

    bll utilisateur

--- src/main/java/fr/eni/ProjetEncheres/DAL/JDBC/EnchereDAOImpl.java
+++ src/main/java/fr/eni/ProjetEncheres/DAL/JDBC/EnchereDAOImpl.java
@@ -57,7 +57,7 @@ public class EnchereDAOImpl implements EnchereDAO{
 			pstmt.executeUpdate();
 				
 		} catch (SQLException e) {
-			throw new DALException("Delete enchère failed - id=" + noEnchere, e);
+			throw new DALException("Delete enchï¿½re failed - id=" + noEnchere, e);
 		} 
 		
 	}
@@ -94,8 +94,8 @@ public class EnchereDAOImpl implements EnchereDAO{
 
 			Enchere enchereAjout = null;
 			while (rs.next()) {
-				// utilisation du conscruteur Categorie
-				//enchereAjout = new Enchere(rs.getLocalDate("date_enchere"), rs.getInt("montant_enchere"), rs.getInt("no_article"), rs.getInt("no_utilisateur"));
+				//utilisation du conscruteur Categorie
+				enchereAjout = new Enchere(rs.getDate("date_enchere").toLocalDate(), rs.getInt("montant_enchere"), rs.getInt("no_article"), rs.getInt("no_utilisateur"));
 				listeEnchere.add(enchereAjout);
 			}
 
