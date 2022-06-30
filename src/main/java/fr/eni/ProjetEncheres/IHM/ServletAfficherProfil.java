b963048aefb97c8cc3e9a3fae718eca04bd2ca5e
caudo2022 <caudo2022@12SE14-BFMRZS2.ad.campus-eni.fr>
2022-06-30 17:13:08 +0200

    Servlet+js afficher profil

--- src/main/java/fr/eni/ProjetEncheres/IHM/ServletAfficherProfil.java
+++ src/main/java/fr/eni/ProjetEncheres/IHM/ServletAfficherProfil.java
@@ -16,11 +16,15 @@ import fr.eni.ProjetEncheres.BO.Utilisateur;
 @WebServlet("/ServletAfficherProfil")
 public class ServletAfficherProfil extends HttpServlet {
 
-	/*private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 1L;
    
 	private UtilisateurManager utilisateurMger;
 	
-    
+	public void init () throws ServletException {
+		utilisateurMger = UtilisateurManager.getInstance();
+		super.init();
+	}
+	
     public ServletAfficherProfil() {
         super();
        
@@ -28,36 +32,39 @@ public class ServletAfficherProfil extends HttpServlet {
 
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		//affiche la page 
 		request.setAttribute(getServletName(), response);
+	/*	String pseudoUtil = request.getParameter("pseudo");
+		try {
+			Utilisateur u = utilisateurMger.postUser(u.getNoUtilisateur());
+
+			request.setAttribute("pizza", p);
+			System.out.println("Pizza: " + p);
+
+			request.setAttribute("piutilisateurs", utilisateurMger.getAllPizzas());
+		} catch (BLLException e) {
+			req.setAttribute("erreur", "Une erreur inattendue est survenue !!");
+			e.printStackTrace();
+		}*/
+
+		request.getRequestDispatcher("/WEB-INF/pages/AfficherProfil.jsp").forward(request, response);
 		this.getServletContext().getRequestDispatcher("/WEB-INF/pages/AfficherProfil.jsp").forward(request, response);
 	}
 
 	
-	/*protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		String pseudo = request.getParameter("pseudo");
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		/*String pseudo = request.getParameter("pseudo");
 		String nom = request.getParameter("nom");
 		String prenom = request.getParameter("prenom");
 		String email = request.getParameter("email");
 		String telephone = request.getParameter("telephone");
 		String rue = request.getParameter("rue");
 		String codePostal = request.getParameter("codePostal");
-		String ville = request.getParameter("ville");
+		String ville = request.getParameter("ville");*/
 		
 	
 		 
-		String pseudoUtil = request.getParameter("pseudo");
-		try {
-			Utilisateur u = utilisateurMger.postUser(noUtilsateur);
-
-			/*req.setAttribute("pizza", p);
-			System.out.println("Pizza: " + p);
-
-			req.setAttribute("pizzas", pizzaMger.getAllPizzas());
-		} catch (BLLException e) {
-			req.setAttribute("erreur", "Une erreur inattendue est survenue !!");
-			e.printStackTrace();
-		}
-
-		request.getRequestDispatcher("/WEB-INF/pages/AfficherProfil.jsp").forward(request, response);*/
+		
 	}
+}
 
--- src/main/webapp/WEB-INF/pages/AfficherProfil.jsp
+++ src/main/webapp/WEB-INF/pages/AfficherProfil.jsp
@@ -4,12 +4,28 @@
 <!DOCTYPE html>
 <html>
 <head>
-<meta charset="UTF-8">
-<title>Insert title here</title>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="boostraps/bootstrap.min.css" />
+    <link rel="stylesheet" href="Sidentifier.css" />
 </head>
 <body>
+ <header>
+        <div class="container-fluid">
+            <div class="col 4 mx-auto text-center">
+             <img src="img/eni.png" alt="eni" id="imgEni" class="mx-auto">
+            
+
+            </div>
+        </div>
+    </header>
 	
 	<form action="/ServletAfficherProfil" method="get">
+	<div class="l">
+    <div class="col-3 col-md-3 col-sm-10  mx-auto text-center">
+        <div class="mb-3">
 	<div>
 		<label for="pseudo">Pseudo : </label>
 		<input type="text" name="pseudo" id="pseudo">
@@ -42,6 +58,9 @@
 		<label for="ville">Ville : </label>
 		<input type="text" name="ville" id="ville">
 	</div>
+	   </div>
+    
+    </div>
 	
 		<c:forEach var="utilisateur" items="${utilisateurs}">
 			<tr>
